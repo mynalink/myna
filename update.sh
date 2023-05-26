8e1ba65a854c9b453189c7a070452be562b84b51
scisaga <scsiaga@gmail.com>
2023-05-27 02:59:21 +0800

    更新配置文件

--- update.sh
+++ update.sh
@@ -4,6 +4,9 @@ docker pull myna/node:v1
 docker pull myna/chrome:v1
 docker pull myna/web:v1
 
+truncate -s 0 $(docker inspect --format='{{.LogPath}}' myna-node)
+truncate -s 0 $(docker inspect --format='{{.LogPath}}' myna-web)
+
 docker rm $(docker stop $(docker ps -a -q --filter name="chrome" --format="{{.ID}}"))
 
 docker-compose -f single-node.yaml up -d
