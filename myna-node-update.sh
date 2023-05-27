771eaebe0d6f860e28604dea5d315a1b14eca11e
scisaga <scsiaga@gmail.com>
2023-05-27 08:06:06 +0800

    更新配置文件

---
+++ myna-node-update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker pull myna/node:v1
+docker pull myna/chrome:v1
+docker pull myna/web:v1
+
+truncate -s 0 $(docker inspect --format='{{.LogPath}}' myna-node)
+truncate -s 0 $(docker inspect --format='{{.LogPath}}' myna-web)
+
+docker rm $(docker stop $(docker ps -a -q --filter name="chrome" --format="{{.ID}}"))
+
+docker-compose -f myna-node.yaml --env-file .env up -d
