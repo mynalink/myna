d875057e37034738201d4b40798f9bb7632c52ba
scisaga <scsiaga@gmail.com>
2023-06-15 11:27:22 +0800

    更新配置文件

--- misc/wg/install_client.sh
+++ misc/wg/install_client.sh
@@ -3,12 +3,12 @@
 # Copy wg0.conf --> ./
 
 mkdir /etc/wireguard && \
-cp $(pwd)/wg0.conf /etc/wireguard/ && \
+cp $(pwd)/wg6.conf /etc/wireguard/ && \
 apt-get update -y && \
 apt-get install wireguard resolvconf -y && \
 echo "net.ipv4.ip_forward = 1
 net.ipv6.conf.all.forwarding = 1" > /etc/sysctl.d/wg.conf && \
 sysctl --system && \
-systemctl enable wg-quick@wg0 && \
-systemctl start wg-quick@wg0 && \
-systemctl status wg-quick@wg0
+systemctl enable wg-quick@wg6 && \
+systemctl start wg-quick@wg6 && \
+systemctl status wg-quick@wg6
